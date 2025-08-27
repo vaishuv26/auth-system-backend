bd566c42d944fd0ed6bcb0048dbd1481ec5df0bc
vaishuv26 <67057538+vaishuv26@users.noreply.github.com>
2025-08-27 15:11:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+
 require('dotenv').config();
 
 const app = express();
